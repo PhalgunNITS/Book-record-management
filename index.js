e6948a0413528d536f6227cf5feb89351babbcde
Phalgun Sharma <phalgun608@gmail.com>
2023-09-13 16:59:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const dotenv = require('dotenv');
+
+
+// Importing routes
+const DbConnection = require("./databaseConnection");
+const usersRouter = require("./routes/users")
+const booksRouter = require("./routes/books")
+
+
+dotenv.config();
 
 const app = express();
+
+DbConnection();
 
 const PORT = 8082;
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        massage: "Server is up and running",
+    });
+});
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+
+
 
 app.get('*', (req, res) => {
     res.status(404).json({
@@ -14,4 +37,4 @@ app.get('*', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
-})
+});
